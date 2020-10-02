cbc2be4cda56b86fbefbde71685d477dffd7d171
Zhen <zhengzhen048@gmail.com>
2020-10-02 23:30:21 +1000

    Reconstruct the flask_blog app

--- flaskblog/users/forms.py
+++ flaskblog/users/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf.file import FileField, FileAllowed
 from flask_wtf import FlaskForm
 from flask_login import current_user
-from wtforms import StringField, PasswordField, SubmitField, BooleanField, TextAreaField
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from flaskblog.models import User
 
@@ -51,12 +51,6 @@ class UpdateAccountForm(FlaskForm):
                 raise ValidationError('Email has been used')
 
 
-class PostForm(FlaskForm):
-    title = StringField('Title', validators=[DataRequired()])
-    content = TextAreaField('Content', validators=[DataRequired()])
-    submit = SubmitField('Post')
-
-
 class RequestResetForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired(), Email()])
     submit = SubmitField('Request Password Reset')
@@ -64,7 +58,6 @@ class RequestResetForm(FlaskForm):
     def validate_email(self, email):
         user = User.query.filter_by(email=email.data).first()
         if not user:
-            # raise ValidationError('If the email exist, you will receive a confirmation email soon.')
             return None
 
 
