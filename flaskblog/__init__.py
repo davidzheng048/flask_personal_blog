cfbb5f68c6ea1b98020d130dea942c88d8a07593
Zhen <zhengzhen048@gmail.com>
2020-10-04 11:29:08 +1000

    Allows blog app to create db automatically

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -15,18 +15,34 @@ login_manager.login_message_category = 'info'
 def create_app(config_class=Config):
     app = Flask(__name__)
 
-    db.init_app(app)
-    bcrypt.init_app(app)
-    login_manager.init_app(app)
-
     from flaskblog.users.routes import users
     from flaskblog.posts.routes import posts
     from flaskblog.main.routes import main
     from flaskblog.errors.handlers import errors
+
     app.config.from_object(Config)
     app.register_blueprint(users)
     app.register_blueprint(posts)
     app.register_blueprint(main)
     app.register_blueprint(errors)
+
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
+
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+
+    # from flaskblog.users.routes import users
+    # from flaskblog.posts.routes import posts
+    # from flaskblog.main.routes import main
+    # from flaskblog.errors.handlers import errors
+
+    # app.config.from_object(Config)
+    # app.register_blueprint(users)
+    # app.register_blueprint(posts)
+    # app.register_blueprint(main)
+    # app.register_blueprint(errors)
 
     return app
