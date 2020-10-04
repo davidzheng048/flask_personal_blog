27874cf84685d240bb828cc06a951b3240b28eb4
Zhen <zhengzhen048@gmail.com>
2020-10-04 17:33:30 +1100

    remove the parts involves user avatar

--- flaskblog/users/routes.py
+++ flaskblog/users/routes.py
@@ -52,9 +52,6 @@ def logout():
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
-        if form.picture.data:
-            picture_file = save_picture(form.picture.data)
-            current_user.image_file = picture_file
 
         current_user.username = form.username.data
         current_user.email = form.email.data
@@ -66,16 +63,7 @@ def account():
         form.username.data = current_user.username
         form.email.data = current_user.email
 
-    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
-    return render_template('account.html', title='Account', image_file=image_file, form=form)
-
-
-@users.route("/user/<string:username>")
-def user_posts(username):
-    page = request.args.get('page', 1, type=int)
-    user = User.query.filter_by(username=username).first_or_404()
-    posts = Post.query.filter_by(author=user).order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
-    return render_template('user_posts.html', posts=posts, user=user)
+    return render_template('account.html', title='Account', form=form)
 
 
 @users.route("/reset_password", methods=['GET', 'POST'])
