3b9befebe7b5b8f230c9dbdfc3b2e74297a550d4
Zhen <zhengzhen048@gmail.com>
2020-10-30 18:34:12 +1100

    Did the following changes:
    1. Move the toggle button to the upper left corner and put a post search form on its previous position
    
    2. Make a route and template for the search feature
    
    3. Slightly change the layout for right sidebar

--- README.md
+++ README.md
@@ -1,9 +1,10 @@
 ###About the Blog
-This blog is mainly based on Corey Schafer's flask tutorial on Youtube.
-After finishing the tutorial, I made some custom changes to the web app, and remake the 
-layout.
 
-Since I don't know much about css, the current looking of the blog is kind of ... awful, 
-but at least it works. I will change it after I study some front-end framework anyway.
+This blog is mainly based on Corey Schafer's flask tutorial on Youtube.
+After completing the tutorial, I have made some custom changes to the web app, such as a category model,
+a search bar and a left sidebar.
+
+Since I don't know much about css, the current looking of the blog is kind of messy, I will change it
+after I learn more about the front-end.
  
 Now the blog is deployed on http://www.buddylich.com, feel free to pay a visit.
--- flaskblog/main/routes.py
+++ flaskblog/main/routes.py
@@ -1,5 +1,4 @@
-from flask import Blueprint
-from flask import render_template, request
+from flask import render_template, request, redirect, Blueprint, url_for
 from flaskblog.models import Post, Category, AccessCount
 from flask_login import current_user
 from flaskblog.main.utils import update_and_init_access_count
@@ -26,11 +25,25 @@ def future_feature():
     return render_template('main/future_feature.html', title='待完成的功能')
 
 
+@main.route("/search", methods=['GET', 'POST'])
+def search():
+    keyword = request.args.get('keyword')
+    page = request.args.get('page', 1, type=int)
+
+    if not keyword:
+        return redirect(url_for('main.home'))
+
+    posts = Post.query.filter(Post.title.contains(keyword)).order_by(Post.date_posted.desc()).paginate(page=page, per_page=10)
+    post_count = Post.query.filter(Post.title.contains(keyword)).count()  # If switch to postgres then stop using this
+
+    return render_template('main/search.html', posts=posts, keyword=keyword, post_count=post_count, page=page)
+
+
 @main.app_context_processor
 def context_processor():
     categories_sorted_by_sequence = Category.query.order_by(Category.sequence.desc()).all()
     categories_sorted_by_id = Category.query.order_by(Category.id.asc()).all()
-    recent_posts = Post.query.order_by(Post.id.desc()).limit(5).all()
+    recent_posts = Post.query.order_by(Post.id.desc()).limit(3).all()
     access_count = AccessCount.query.first().count
     return {
         'categories_sorted_by_sequence': categories_sorted_by_sequence,
